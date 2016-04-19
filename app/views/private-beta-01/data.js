17591fafa832fe5406059a7ef7865d46e7108a2e
Andy Mantell <andymantell@hotmail.com>
2016-04-19 10:20:31 +0100

    Slight restructure of search

--- app/views/private-beta-01/data.js
+++ app/views/private-beta-01/data.js
@@ -1,74 +1,21 @@
-var results = require('../../../data/generate');
+var data = require('../../../data/titles');
+var search = require('../../../data/search');
+
+// Use this space to insert any prototype specific entries onto the available
+// set of data.
+// For example:
+data.push({
+  data: {},
+  title_number: 'FAKE123123',
+  address: [
+    'Seaton Court',
+    '2',
+    'William Prance Rd',
+    'Plymouth',
+    'PL6 5WS'
+  ]
+});
 
 module.exports = function(searchTerm, callback) {
-  searchTerm = searchTerm.trim();
-
-  var terms = searchTerm.split(/[\s,]+/);
-  terms.forEach(function(term, index) {
-    terms[index] = term.toString().toLowerCase();
-  });
-
-  var searchResults = [];
-  var logEntry;
-  var searchType;
-
-  // If it looks like a title number, do a title number search
-  if(searchTerm.length === 11 && searchTerm.indexOf('FAKE') !== -1 && searchTerm.indexOf(' ') === -1) {
-    logEntry = 'Searching for title number: ' + searchTerm;
-    searchType = 'title_number';
-  } else {
-    logEntry = 'Searching for address: ' + searchTerm;
-    searchType = 'address';
-  }
-
-  console.time(logEntry);
-
-  results.forEach(function(result) {
-    result.score = 0;
-
-    // // If it looks like a title number, do a title number search
-    if(searchType === 'title_number') {
-
-      if (result.title_number.toLowerCase() === searchTerm.toLowerCase()) {
-        searchResults.push(result);
-      }
-
-    } else {
-
-      // Otherwise do an address search
-      result.address.forEach(function(addressItem) {
-        var compareItem = addressItem.toString().toLowerCase();
-
-        terms.forEach(function(term) {
-          var pos = compareItem.indexOf(term);
-
-          if(pos !== -1) {
-            result.score += (compareItem.length - pos) / compareItem.length;
-          }
-        })
-      });
-
-      if(result.score > 0) {
-        searchResults.push(result);
-      }
-
-    }
-  });
-
-  searchResults = searchResults.sort(function(a, b) {
-
-    // Initially sort by score
-    if(a.score < b.score) return 1;
-    if(a.score > b.score) return -1;
-
-    // Otherwise sort by number
-    if(a.address[0] < b.address[0]) return -1;
-    if(a.address[0] > b.address[0]) return 1;
-
-    return 0;
-  });
-
-  console.timeEnd(logEntry);
-  callback(searchResults);
-
+  search(data, searchTerm, callback);
 };
---
+++ data/search.js
@@ -0,0 +1,86 @@
+module.exports = function(data, searchTerm, callback) {
+  searchTerm = searchTerm.trim();
+
+  var searchResults = [];
+  var logEntry;
+  var searchType;
+
+  // Split the search term into pieces so we can compare each bit
+  // (Split by spaces and by commas)
+  var terms = searchTerm.split(/[\s,]+/);
+  terms.forEach(function(term, index) {
+    terms[index] = term.toString().toLowerCase();
+  });
+
+  // If it looks like a title number, do a title number search
+  if(searchTerm.length === 11 && searchTerm.indexOf('FAKE') !== -1 && searchTerm.indexOf(' ') === -1) {
+    logEntry = 'Searching for title number: ' + searchTerm;
+    searchType = 'title_number';
+  } else {
+    logEntry = 'Searching for address: ' + searchTerm;
+    searchType = 'address';
+  }
+
+  console.time(logEntry);
+
+  // Loop over the complete set of results
+  data.forEach(function(result) {
+
+    // Keep track of a "score" for each result which we will sort by later
+    result.score = 0;
+
+    // // If it looks like a title number, do a title number search
+    if(searchType === 'title_number') {
+
+      if (result.title_number.toLowerCase() === searchTerm.toLowerCase()) {
+        searchResults.push(result);
+      }
+
+    } else {
+
+      // Otherwise do an address search
+      // Loop over each component of the address
+      result.address.forEach(function(addressItem) {
+        var compareItem = addressItem.toString().toLowerCase();
+
+        // Loop over each component of the search term (Split by commas and spaces earlier)
+        terms.forEach(function(term) {
+
+          // Find out position of the search term in this address item
+          var pos = compareItem.indexOf(term);
+
+          // If we've found it
+          if(pos !== -1) {
+            // Assign it a score based on the position within the string
+            result.score += (compareItem.length - pos) / compareItem.length;
+          }
+        })
+      });
+
+      // If we've got a non zero score, pop it onto the set of results to return
+      if(result.score > 0) {
+        searchResults.push(result);
+      }
+
+    }
+  });
+
+  // Sort the resutls
+  searchResults = searchResults.sort(function(a, b) {
+
+    // Initially sort by score
+    if(a.score < b.score) return 1;
+    if(a.score > b.score) return -1;
+
+    // Otherwise sort by the first component of the address
+    if(a.address[0] < b.address[0]) return -1;
+    if(a.address[0] > b.address[0]) return 1;
+
+    return 0;
+  });
+
+  console.timeEnd(logEntry);
+
+  callback(searchResults);
+
+};
